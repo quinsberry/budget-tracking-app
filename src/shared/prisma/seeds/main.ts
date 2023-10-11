df60c9800bf5d54a478117c90f77d988bba2a03e
ekolesnyk <eugene.kolesnyk@customertimes.com>
2023-10-11 12:25:17 +0200

    refactored seeding process

--- src/shared/prisma/seeds/main.ts
+++ src/shared/prisma/seeds/main.ts
@@ -1,38 +1,76 @@
-import { PrismaClient } from '@prisma/client';
-import { PasswordService } from '../../services/password.service';
-import { users, transactionTags } from './data';
-import { UsersService } from '../../../v1/users/users.service';
-import { PrismaService } from '../prisma.service';
+import { NestFactory } from '@nestjs/core';
+import { AvailableBank, PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
-const prismaService = new PrismaService();
-const passwordService = new PasswordService();
-const usersService = new UsersService(prismaService);
+import { AppModule } from '@/app.module';
+import { PasswordService } from '@/shared/services/password.service';
+import { CardsService } from '@/v1/cards/cards.service';
+import { CreateMonobankCardDto } from '@/v1/cards/dto/create-monobank-card.dto';
+import { CreateUserDto } from '@/v1/users/dto/create-user.dto';
+import { UsersService } from '@/v1/users/users.service';
 
-async function seedUsers(): Promise<any> {
-    return Promise.all(users.map(async u => {
-        const hashedPassword = await passwordService.hashPassword(u.password);
-        return usersService.create({
-            ...u,
-            password: hashedPassword,
-        });
-    }));
+import { users } from './data';
+
+interface CustomSeedUser extends CreateUserDto {
+    cards: CustomSeedCard[];
+}
+interface CustomSeedCard extends CreateMonobankCardDto {
+    bank: AvailableBank;
 }
 
 async function main() {
-    console.log(`Start seeding...`);
-    Promise.all([
-        seedUsers(),
-    ]);
-    console.log(`Seeding finished.`);
+    NestFactory.createApplicationContext(AppModule).then(appContext => {
+        const prisma = new PrismaClient();
+        const passwordService = new PasswordService();
+        const cardsService = appContext.get(CardsService);
+        const usersService = appContext.get(UsersService);
+
+        async function seedUsers(): Promise<any> {
+            return Promise.all(
+                users.map(async u => {
+                    const hashedPassword = await passwordService.hashPassword(u.password);
+                    return usersService.create({
+                        ...u,
+                        password: hashedPassword,
+                    });
+                })
+            );
+        }
+
+        async function customSeed(init: CustomSeedUser): Promise<any> {
+            const user = await usersService.create({
+                fullName: init.fullName,
+                email: init.email,
+                password: await passwordService.hashPassword(init.password),
+            });
+
+            init.cards.forEach(async card => {
+                if (card.bank === AvailableBank.Monobank) {
+                    cardsService.createMonobankCard(user.id, {
+                        description: card.description,
+                        cardNumber: card.cardNumber,
+                        token: card.token,
+                        startTrackingTime: card.startTrackingTime,
+                    });
+                }
+            });
+        }
+
+        return seedUsers()
+            .then(() => console.log('Users are seeded'))
+            .then(() => {
+                console.debug('Seeding complete!');
+            })
+            .catch(async error => {
+                console.error('Seeding failed!');
+                console.error(error);
+                await prisma.$disconnect();
+                process.exit(1);
+            })
+            .finally(async () => {
+                appContext.close();
+                await prisma.$disconnect();
+            });
+    });
 }
 
-main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async e => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+main();
