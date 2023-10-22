23363e7db4884fcd5696dc9b671b78a8c57f5e2e
quinsberry <eugeniusz.kolesnyk@gmail.com>
2023-10-22 19:25:55 +0200

    fixed the 500 user validation error when user tried to login

--- src/v1/auth/auth.service.ts
+++ src/v1/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
 
@@ -9,35 +9,41 @@ import { UsersService } from '@/v1/users/users.service';
 @Injectable()
 export class AuthService {
     constructor(
-        private usersService: UsersService,
-        private jwtService: JwtService,
-        private hashService: PasswordService
+        private readonly usersService: UsersService,
+        private readonly jwtService: JwtService,
+        private readonly hashService: PasswordService
     ) {}
 
-    async validateUser(email: string, password: string): Promise<Omit<User, 'password'>> {
+    async validateUser(email: string, password: string): Promise<Omit<User, 'password'> | null> {
         const user = await this.usersService.findByEmail(email);
-        const passwordValid = await this.hashService.comparePassword(user.password, password);
-        if (user && passwordValid) {
-            const { password, ...result } = user;
-            return result;
+        if (user) {
+            const passwordValid = await this.hashService.comparePassword(user.password, password);
+            if (passwordValid) {
+                const { password, ...result } = user;
+                return result;
+            }
         }
         return null;
     }
 
     async register(dto: CreateUserDto) {
+        const existedUser = await this.usersService.findByEmail(dto.email);
+        if (existedUser) {
+            throw new ConflictException('The user with this email already exists');
+        }
         const hashedPassword = await this.hashService.hashPassword(dto.password);
         const user = await this.usersService.create({
             ...dto,
             password: hashedPassword,
         });
         return {
-            token: this.jwtService.sign({ id: user.id }),
+            access_token: this.jwtService.sign({ id: user.id }),
         };
     }
 
     async login(user: User) {
         return {
-            token: this.jwtService.sign({ id: user.id }),
+            access_token: this.jwtService.sign({ id: user.id }),
         };
     }
 }
