b9b23352cf49db23a46ebb97261e49b13db59867
quinsberry <eugeniusz.kolesnyk@gmail.com>
2023-10-27 00:23:49 +0200

    fixed get all users route

--- src/v1/users/users.controller.ts
+++ src/v1/users/users.controller.ts
@@ -1,11 +1,7 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
-
-import { UserId } from '@/shared/decorators/user-id.decorator';
-import { exclude } from '@/shared/prisma/utils/exclude';
+import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller()
 @ApiTags('users')
@@ -15,14 +11,6 @@ export class UsersController {
     @Get()
     async getAll() {
         const users = await this.usersService.getAll();
-        return users.map(user => exclude(user, ['password']));
-    }
-
-    @Get('/me')
-    @ApiBearerAuth()
-    @UseGuards(JwtAuthGuard)
-    async getMe(@UserId() id: string) {
-        const user = await this.usersService.findById(id);
-        return exclude(user, ['password']);
+        return users.map(user => user.fullName);
     }
 }
